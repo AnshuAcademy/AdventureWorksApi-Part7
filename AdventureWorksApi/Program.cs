3c58f69fdbe833e8be68626b5470778235ca2f9e
Naveen Tg <naveen.tg@championx.com>
2022-09-03 19:24:07 +0530

    Initial commit

---
+++ AdventureWorksApi/Program.cs
@@ -0,0 +1,41 @@
+using AdventureWorksPersistence.DataAccess;
+using AdventureWorksPersistence.Models;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+                .AddFluentValidation(opts => opts.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddTransient<IAdventureWorksDataAccess, AdventureWorksDataAccess>();
+
+builder.Services.AddDbContext<AdventureWorksDBContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDBConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
